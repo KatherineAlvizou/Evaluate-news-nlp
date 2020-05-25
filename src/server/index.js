16fcce1303edb2e307f37f8149a27b274073e869
Katerina Alvizou <katherine.alvizou@gmail.com>
2020-05-25 17:05:51 +0200

    cors and body-parser packages

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
 var path = require('path')
-const express = require('express')
+
 const mockAPIResponse = require('./mockAPI.js')
 
+
+const express = require('express')
 const app = express()
+
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors= require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
